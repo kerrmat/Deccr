adfbbd27297239659f7dfa955cdf672c54ae57e7
unknown <kerrmat@oregonstate.edu>
2019-08-12 23:33:08 -0700

    Bitch Dayub

--- Driver.java
+++ Driver.java
@@ -1,13 +1,14 @@
-import java.util.*
-import java.awt.*
-import java.swing.*
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-
-public class DectalkUI{
+class DectalkUI{
 	
 	JFrame frame = new JFrame();
+	JButton[] buttonarray = new JButton[5];
 		
-	DectalkUI(){
+	public DectalkUI(){
 		
 		buildUI();
 		
@@ -22,16 +23,55 @@ public class DectalkUI{
 		frame.setBounds(200,200,400,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
 		JButton button = new JButton("Sample Text");
-		button.setBounds(130,200,100,40);
+		buttonarray[0] = button;
 		
-		frame.add(button);
+		buttonarray[0].setBounds(130,200,150,40); //x,y,width,height
+		buttonarray[0].addMouseListener(new ExpandButton());
+		
+		frame.add(buttonarray[0]);
 		
 	}
-	
-	
 }
+
+class ExpandButton implements MouseListener{
+	public boolean pressed = false;
+	int oldx = 0;
+	int oldy = 0;
+	int newx;
+	int newy;
+	
+	public ExpandButton(){	
+	}
+	
+	public void mousePressed(MouseEvent e){
+		oldx = (int)MouseInfo.getPointerInfo().getLocation().getX();
+		oldy = (int)MouseInfo.getPointerInfo().getLocation().getY();
+		pressed = true;
+	}
+	public void mouseReleased(MouseEvent e){
+		newx = (int)MouseInfo.getPointerInfo().getLocation().getX();
+		newy = (int)MouseInfo.getPointerInfo().getLocation().getY();
+		pressed = false;
+		setButtonSize(e);
+	}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+	public void mouseClicked(MouseEvent e){}
+	
+	public void setButtonSize(MouseEvent e){
+		
+		JComponent btn = (JComponent)e.getSource();
+		
+		int width = btn.getWidth() + newx - oldx;
+		int height = btn.getHeight() - newy + oldy;
+		int y = btn.getY() + newy - oldy;
+		btn.setBounds(btn.getX(),y,Math.max(width,0),Math.max(height,0));
+	}
+}
+	
+	
+
 
 public class Driver {
 	
