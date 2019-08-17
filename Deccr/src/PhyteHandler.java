b72e48b2b2e08b2c7efa89320cc59a094f360cfa
Andres Giesemann <andresrgiesemann@gmail.com>
2019-08-16 21:18:44 -0700

    eclipse work pls

---
+++ Deccr/src/PhyteHandler.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class PhyteHandler {
+	
+	public ArrayList<Phyte> PhyteList;
+	
+	public PhyteHandler() {
+		
+		 PhyteList = new ArrayList<Phyte>();
+		
+	}
+	
+	public String toStringArray() {
+		String ret = "";
+		for (int i=0; i < PhyteList.size(); i++) {
+			
+			ret += PhyteList.get(i).toString();
+			
+		}
+		
+		return ret;
+		
+		
+	}
+	
+	public String testPhyteHandler() {
+				
+	}
+	
+}
+
+class Phyte {
+	
+	public String phoneme;
+	public int duration;
+	public int pitch;
+	
+	public Phyte(){
+		phoneme = "aa";
+		duration = 1000;
+		pitch = 15;
+	}
+	
+	public Phyte(String pho, int dur, int pit) {
+		phoneme = pho;
+		duration = dur;
+		pitch = pit;
+	}
+	
+	public String toString() {
+		return "[" + phoneme + "<" + duration + "," + pitch + ">]";
+	}
+	
+}
